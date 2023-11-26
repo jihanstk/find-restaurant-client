2b73ba770dd09b89f4d2d36eaa8c727a6380080c
jihanstkm <jihanstk@gmail.com>
2023-11-27 02:01:49 +0600

    content Hover Effect

--- src/Components/Content/SingleTabPanel.jsx
+++ src/Components/Content/SingleTabPanel.jsx
@@ -2,7 +2,7 @@ import "react-tabs/style/react-tabs.css";
 
 const SingleTabPanel = ({ restaurant }) => {
   return (
-    <div className="card w-auto bg-transparent border border-[#19A7CE] text-[#C5FFF8] content-shadow">
+    <div className="card w-auto bg-transparent hover:bg-[#000]/10 border border-[#19A7CE] hover:border-[#91e5fd] duration-500 text-[#C5FFF8] content-shadow">
       <div className="card-body">
         <h2 className="card-title">{restaurant.restaurant_name}</h2>
         <p className="text-left">{restaurant.state}</p>
